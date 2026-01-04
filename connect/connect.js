3d1077bccfb4a316f1510d482b7f539bd09ac640
beardking <barbosa_leandro@rocketmail.com>
2026-01-04 12:09:04 -0300

    env connect

--- connect/connect.js
+++ connect/connect.js
@@ -1,8 +1,8 @@
 const conexao = require('mysql2');
 const db = conexao.createPool({
-    host: 'localhost',
-    user:  'root',
-    password: 'password',
-    database: 'model_site'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'model_site'
 });
 module.exports = db;
